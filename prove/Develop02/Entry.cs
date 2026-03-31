f5f2018d613ef19df2ce03336b10424f3cdd98b2
htwilson242 <htwilson242@gmail.com>
2026-03-31 15:13:21 -0600

    entry.cs constructor built

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Net;
 public class Entry
 {
     public string _date;
     public string _promptText;
     public string _entrytext;
     public string _mood;
+
+    public entry(string date, string _promptText, string _entrytext, string _mood)
+    Entry entry = new Entry(date, _promptText, WebResponse, _mood);
 }
