9633139b46523e48438c5d4e192a50dbbff27271
htwilson242 <htwilson242@gmail.com>
2026-04-02 15:05:54 -0600

    Program.cs Console menu options developed

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,5 +7,25 @@ class Program
     {
         Journal journal = new Journal();
         PromptGenerator promptGenerator = new PromptGenerator();
+
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
+            Console.WriteLine("5. Search");
+            Console.WriteLine("6. Quit");
+            Console.Write("Choose an option: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            
+            
+        }
     }
 }
