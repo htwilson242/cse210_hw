a6996d11b14ad0703d6fcd2045b696184db08969
htwilson242 <htwilson242@gmail.com>
2026-04-02 15:11:51 -0600

    Program.cs input  results set up, going to adjust results and add creativity element to this file

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -22,9 +22,65 @@ class Program
             Console.Write("Choose an option: ");
 
             string choice = Console.ReadLine();
+            Console.WriteLine();
 
-            switch (choice)
-            
+            if (choice == "1")
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+                Console.WriteLine($"Prompt: {prompt}");
+                Console.Write("> ");
+
+                string response = Console.ReadLine();
+
+                Entry entry = new Entry();
+                entry._date = DateTime.Now.ToShortDateString();
+                entry._promptText = prompt;
+                entry._entrytext = response;
+
+                journal.AddEntry(entry);
+
+                Console.WriteLine("Entry added to journal succesfully");
+            }
+            else if (choice == "2")
+            {
+                journal.DisplayAll();
+            }
+            else if (choice == "3")
+            {
+                Console.Write("Enter filename");
+                string filename = Console.ReadLine();
+                
+                if (File.Exists(filename))
+                {
+                    journal.LoadFromFile(filename);
+                    Console.WriteLine("Journal loaded successfully");
+                }
+                else
+                {
+                    Console.WriteLine("File not found.");
+                }
+            }
+            else if (choice == "4")
+            {
+                Console.Write("Enter filename to load: ");
+                string filename = Console.ReadLine();
+                journal.LoadFromFile(filename);
+                Console.WriteLine("Journal loaded successfully");
+            }
+            else if (choice == "5")
+            {
+                Console.Write("Enter keyword to search: ");
+                string keyword = Console.ReadLine();
+                journal.SearchEntries(keyword);
+            }
+            else if (choice == "6")
+            {
+                running = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid option. Please try again.");
+            }
             
         }
     }
